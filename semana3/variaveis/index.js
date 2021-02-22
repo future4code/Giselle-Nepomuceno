64a6ab4da950b9f91f84383c902f25e65a07345e
Giselle Rosa <gisellenrosa@gmail.com>
2021-02-22 14:09:57 -0300

    Aula 10

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,92 @@
+/* INTERPRETAÇÃO 1
+
+a = 10
+b = 10
+
+console.log(b)
+Resposta: 10
+
+b = 5
+console.log(a, b)
+Resposta: 10, 5 
+*/
+
+/* INTERPRETAÇÃO 2 
+a = 10
+b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c) 
+Resposta: 10, 10, 10
+ */
+
+/* Exercicio de escrita 1
+a) let nome 
+b) let idade
+c) console.log(typeof idade, typeof nome)
+d) O resultado foi undefined, 
+pois eu não defini o valor para ser encontrado o type 
+Utilizei:
+d) *tem 2 letras d no exercicio
+let nome = prompt('Qual seu nome?') 
+let idade = prompt('Qual a sua idade?')
+console.log(nome, idade)
+
+e)  Notei que ambos são considerados string.
+let nome = prompt('Qual seu nome?') 
+let idade = prompt('Qual a sua idade?')
+console.log(typeof nome, typeof idade)
+
+f)
+let nome = prompt('Qual seu nome?') 
+let idade = prompt('Qual a sua idade?')
+console.log("Olá", nome, "você tem", idade)
+
+
+*/
+
+
+/* Exercicio de escrita 2
+let nome = prompt('Qual seu nome?') 
+let idade = prompt('Qual a sua idade?')
+let cidade = prompt('Onde você mora?')
+let serie= prompt('Qual sua serie favorita?')
+let cor = prompt('Qual sua cor favorita?')
+
+console.log(nome, "você tem", idade, "anos", "você mora no(a)", cidade, "sua serie favorita é", serie, "sua cor favorita é", cor )
+
+*/
+
+/* Exercicio de escrita 3
+a) let comidas = [ "Nhoque","Yakisoba de camarão", "Miojo", "Alcatra", "Batata frita" ]
+console.log(comidas)
+
+b) let comidas = [ "Nhoque","Yakisoba de camarão", "Miojo", "Alcatra", "Batata frita" ]
+console.log("Essas são minhas comidas favoritas:")
+console.log(comidas[0])
+console.log(comidas[1])
+console.log(comidas[2])
+console.log(comidas[3])
+console.log(comidas[4])
+
+desafio:
+let comida = prompt("qual sua comida favorita?")
+let comidas = [ "Nhoque","Yakisoba de camarão", "Miojo", "Alcatra", "Batata frita" ]
+console.log("Essas são minhas comidas favoritas:")
+console.log(comidas[0])
+console.log(comida)
+console.log(comidas[2])
+console.log(comidas[3])
+console.log(comidas[4])
+*/
+
+/* Exercicio de escrita 4
+let perguntas = ['Você é maior de 18 anos?','Você é Mulher?', 'Você esta usando roxo?']
+let respostas = [ true, true, false]
+console.log( perguntas[0], respostas[0])
+console.log( perguntas[1], respostas[1])
+console.log( perguntas[2], respostas[2])
+
+*/
