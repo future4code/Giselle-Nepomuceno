af519cb9e79ab23684e9b606cb928192979d4bdb
Giselle Rosa <gisellenrosa@gmail.com>
2021-02-26 15:15:09 -0300

    projeto

---
+++ semana3/blackjack/asas.js
@@ -0,0 +1,24 @@
+const baralho =  ['As', 2, 3, 4, 5, 6, 7, 8, 9, 10 ,'J' ,'Q' ,'K']
+const naipe = ['Copas ♥️', 'Paus ♣️', 'Ouros ♦️', 'Espadas ♠️']
+
+// let randomBaralho = baralho[Math.floor(Math.random() * baralho.length)];
+// let randomNaipe = naipe[Math.floor(Math.random() * naipe.length)];
+// console.log(randomBaralho, 'de', randomNaipe)
+
+
+
+// console.log("Bem vindo ao BlackJack Game")
+
+// if(confirm("Quer iniciar uma nova rodada?")) {
+// 	// o que fazer se o usuário clicar "ok"
+//     const carta = comprarCarta(RandomSource(baralho[i])); 
+//     // Sorteia uma carta. Por exemplo, o rei de ouros
+
+//     console.log(carta.texto) 
+//     // imprime o texto da carta. Nesse caso: "K♦️"
+
+//     console.log(carta.valor) 
+//     // imprime o valor da carta (um número). Nesse caso: 10)
+// } else {
+// 	console.log("O jogo acabou :O")
+// }
---
+++ semana3/blackjack/desafio.js
@@ -0,0 +1,58 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+console.log("Bem vindo ao BlackJack Game")
+
+if(confirm("Quer iniciar uma nova rodada?")) {
+	// o que fazer se o usuário clicar "ok"
+    const cartaUsuario = comprarCarta(); 
+    const cartaUsuario2 =comprarCarta(); 
+    const cartaComputador = comprarCarta(); 
+    const cartaComputador2 = comprarCarta(); 
+    const somaUsuario =(cartaUsuario.valor + cartaUsuario2.valor)
+    const somaComputador = (cartaComputador.valor + cartaComputador2.valor)
+
+
+    // Sorteia uma carta. Por exemplo, o rei de ouros
+
+    console.log('Usuário - Cartas:', cartaUsuario.texto, cartaUsuario2.texto, ' - ',  somaUsuario)
+    console.log('Computador - Cartas:', cartaComputador.texto, cartaComputador2.texto,' - ', somaComputador)
+      if(somaUsuario > somaComputador){
+         console.log('O usuário ganhou!')
+      } else if (somaUsuario=== somaComputador){
+         console.log('Empate!')
+      } else {
+         console.log('O computador ganhou!')
+      }
+      if(cartaUsuario === "A" && cartaUsuario2 == "A" || cartaComputador === "A" && cartaComputador2 == "A" ){
+         cartaUsuario = comprarCarta(); 
+         cartaUsuario2 =comprarCarta();
+         cartaComputador = comprarCarta(); 
+         cartaComputador2 = comprarCarta();  
+       }
+      
+      (confirm(
+         "Suas cartas são " + cartaUsuario.texto + ' e '+ cartaUsuario2.texto + " A carta revelada do computador é "+ cartaComputador.texto +
+         "\n"+  // \n faz pular a linha
+         "Deseja comprar mais uma carta?" ))
+         }
+    // imprime o valor da carta (um número). Nesse caso: 10)
+} else {
+	console.log("O jogo acabou :O")
+}
+
+ * 
+ */
+
+
+ 
+
+ 
+
