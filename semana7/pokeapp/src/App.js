4236fd43c4a5da962dd691c85066856af5af8ef2
Giselle Rosa <gisellenrosa@gmail.com>
2021-03-25 17:52:52 -0300

    estilizando

--- semana7/pokeapp/src/App.js
+++ semana7/pokeapp/src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
 import './App.css';
 import axios from "axios";
+import styled from "styled-components"
+
+const ContentPoke = styled.div`
+display:grid; 
+justify-content:center;
+`;
+const CardPoke = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+margin-top: 20px;
+align-items:center;
+border: 1px solid black;
+width: 300px;
+border-radius: 8px;
+font-weight:600;
+`;
 
 export default class App extends React.Component {
   state = {
@@ -18,7 +35,7 @@ export default class App extends React.Component {
 
   getPokemons =  async () => {
       const res = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon?limit=700&offset=0"
+        "https://pokeapi.co/api/v2/pokemon?limit=800&offset=0"
       );
       this.setState({pokemons: res.data.results});
       console.log(res.data.results);
@@ -87,32 +104,70 @@ export default class App extends React.Component {
     })
     
     const SelectionPokemon = (()=>{
+      if (this.state.pokeType === "fire"){
+        this.setState({pokeType: "Fogo"})
+      }else if (this.state.pokeType === "water"){
+        this.setState({pokeType: "Água"})
+      }else if (this.state.pokeType === "bug"){
+        this.setState({pokeType: "Inseto"})
+      }else if (this.state.pokeType === "grass"){
+        this.setState({pokeType: "Planta"})
+      }else if (this.state.pokeType === "Rock"){
+        this.setState({pokeType: "Pedra"})
+      }else if (this.state.pokeType === "psychic"){
+        this.setState({pokeType: "Psíquico"})
+      }else if (this.state.pokeType === "eletric"){
+        this.setState({pokeType: "Elétrico"})
+      }else if (this.state.pokeType === "ice"){
+        this.setState({pokeType: "Gelo"})
+      }else if (this.state.pokeType === "rock"){
+        this.setState({pokeType: "Pedra"})
+      }else if (this.state.pokeType === "ghost"){
+        this.setState({pokeType: "Fantasma"})
+      } else if (this.state.pokeType === "dark"){
+        this.setState({pokeType: "Negro"})
+      }else if (this.state.pokeType === "poison"){
+        this.setState({pokeType: "Venenoso"})
+      }else if (this.state.pokeType === "fairy"){
+        this.setState({pokeType: "Fada"})
+      }else if (this.state.pokeType === "fighting"){
+        this.setState({pokeType: "Luta"})
+      }else if (this.state.pokeType === "normal"){
+        this.setState({pokeType: "Normal"})
+      }else if (this.state.pokeType === "steel"){
+        this.setState({pokeType: "Metálico"})
+      }else if (this.state.pokeType === "flying"){
+        this.setState({pokeType: "Voador"})
+      }else if (this.state.pokeType === "ground"){
+        this.setState({pokeType: "Terrestre"})
+      }else if (this.state.pokeType === "dragon"){
+        this.setState({pokeType: "Dragão"})
+      }
       return <div>
-      <p>Name: {this.state.pokeName}</p> 
-      <p>Base Experience: {this.state.pokeXP} </p>   
-      <p>Type: {this.state.pokeType}</p>
-      <p>Weight: {this.state.pokeWeight/10} kg </p>
+      <p>Nome: {this.state.pokeName}</p> 
+      <p>Experiência Base: {this.state.pokeXP} XP</p>   
+      <p>Tipo: {this.state.pokeType}</p>
+      <p>Peso: {this.state.pokeWeight/10} kg </p>
       </div>
     })
 
     
   return (
-    <div>
+    <ContentPoke>
       <h1>Pokemon</h1>
-      <h3>Tem que pegar! Pokemon!</h3>
+      <h3>Selecione um Pokemon!</h3>
       <select onChange = {this.handlePokemonSelect}>
-        <option>Escolha seu Pokemon</option>
-        {pokemonSelection}
+          {pokemonSelection}
       </select>
       <div>
         {this.state.pokePicture && (
-          <div>
+          <CardPoke>
             <img src={this.state.pokePicture} alt = {"foto do pokemon selecionado"}/>
             <SelectionPokemon></SelectionPokemon>
-          </div>
+          </CardPoke>
         )}
       </div>
-    </div>
+    </ContentPoke>
   );
   }
 }
