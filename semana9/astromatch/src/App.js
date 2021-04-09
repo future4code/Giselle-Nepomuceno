69a15f1327cd2dd9a3e5beebc0fb6aa7b366dc70
Giselle Rosa <gisellenrosa@gmail.com>
2021-04-08 21:30:10 -0300

    Finalização e Estilização

--- semana9/astromatch/src/App.js
+++ semana9/astromatch/src/App.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import "./styles.css";
-import Home from "./pages/Home";
-import Match from "./pages/Match";
+import Match from "./pages/Match/Match";
+import MatchList from "./pages/MatchList/MatchList";
 
 export default function App(props) {
-  const [page, setPage] = useState("Home");
+  const [page, setPage] = useState("Match");
 
   const changePage = (newPage) => {
     setPage(newPage);
   };
 
   const renderPage = () => {
-    if (page === "Home") {
-      return <Home change={changePage} />;
-    } else if (page === "Match") {
+    if (page === "Match") {
       return <Match change={changePage} />;
+    } else if (page === "MatchList") {
+      return <MatchList change={changePage} />;
     }
   };
 
