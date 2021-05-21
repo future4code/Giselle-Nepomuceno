94e3c2ec39adcb4127f21b33a0619c2b73d62bc2
Giselle Rosa <gisellenrosa@gmail.com>
2021-05-21 19:57:11 -0300

    creating an account complete

--- semana15/projeto/src/index.ts
+++ semana15/projeto/src/index.ts
@@ -1,57 +1,78 @@
-import express, {Express, Request, Response} from 'express';
-import cors from 'cors';
-
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
 
 const app: Express = express();
-
 
 app.use(express.json());
 app.use(cors());
 
+type account = {
+  value: number;
+  date: string;
+  description: string;
+};
 type user = {
-name:string,
-cpf:number,
-birth:string ,
-}
-let users: user[] = [{
+  name: string;
+  cpf: number;
+  birth: string;
+  bankBalance: number;
+  extract: account[];
+};
+let users: user[] = [
+  {
     name: "Giselle Rosa",
     cpf: 10101010101,
-    birth:"02/10/1993",
+    birth: "02/10/1993",
+    bankBalance: 0,
+    extract: [],
+  },
+];
+app.post("/labebank/createaccount", (req: Request, res: Response) => {
+    const { name, cpf, birth } = req.body;
 
-}]
-app.post("/labebank/createaccount", (req:Request, res:Response)=>{
-    try{const {name,cpf, birth} = req.body;
-        const newUser: user = {
-            name,
-            cpf,
-            birth
-        }
-        users.push(newUser);
+    let currentYear = new Date().getFullYear();
+    let birthYear = new Date(birth).getFullYear();
+    let age = currentYear - birthYear;
 
-        if (newUser){
-            let birth = newUser.birth
-            let birthYear = birth.substr(6)
-            let currentYear = new Date().getFullYear()
-            let calcYear =  currentYear - Number(birthYear) 
-            console.log(calcYear)
-            if ( calcYear >= 18 ){
-                res.status(200).send({
-                    message: "ok",
-                    user: newUser,
-                });
-            }else {
-                res.status(404).send('Para abrir a conta é necessário ter mais de 18 anos.');
+    try {
+        if(name && cpf && birth){
+            const checkCPFuser = users.find(element => element.cpf === cpf)
+            if(checkCPFuser){
+                throw new Error("This account already exist")
+            }
+            if(age <=18){
+                throw new Error("Necessary to be over 18 to create an account")
             }
         }
-    }catch (err) {
-        res.status(400).send({
-          message: err.message,
+    
+    const newUser: user = {
+      name,
+      cpf,
+      birth,
+      bankBalance: 0,
+      extract: [],
+    };
+    users.push(newUser);
+
+
+    if (newUser) {
+        res.status(200).send({
+          message: "ok",
+          user: newUser,
         });
+      } else {
+        res
+          .status(404)
+          .send("Necessary complete all informations to create an account");
       }
-    }
-})
+    
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
 
-
-app.listen(3003, ()=>{
-    console.log("Server is running at http://localhost:3003");
-}); 
+app.listen(3003, () => {
+  console.log("Server is running at http://localhost:3003");
+});
