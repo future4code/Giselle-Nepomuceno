2adab307c705b01ccf37bef8b8d2d3ded757b426
Giselle Rosa <gisellenrosa@gmail.com>
2021-06-10 16:14:40 -0300

    inciando Projeto

---
+++ Semana17/projeto/src/endpoints/signup.ts
@@ -0,0 +1,11 @@
+import {Request, Response} from "express";
+import connection from "../connection"
+
+
+export default async function signUp(req: Request, res: Response):Promise<void> {
+    try{
+
+    } catch(error){
+
+    } 
+}
